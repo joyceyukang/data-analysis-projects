c6cab8ebf7c0dd8cb5f8e9ca2efdbad72f8e1507
Joyce <joyceyukang@gmail.com>
2023-09-21 17:00:24 -0700

    apple stock info

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
 import yfinance as yf
 import pandas as pd
+import json
 
 apple = yf.Ticker("AAPL")
+
+with open('apple.json') as json_file:
+    apple_info = json.load(json_file)
+    # print the type of data variable
+    print((apple_info))
+# apple_info
