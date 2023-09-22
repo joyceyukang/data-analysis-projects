b192b41542dbc418b8c2e42cb74644b1ae1d8332
Joyce <joyceyukang@gmail.com>
2023-09-21 17:11:58 -0700

    apple stock info

--- data.py
+++ data.py
@@ -1,11 +1,16 @@
 import yfinance as yf
 import pandas as pd
 import json
+import matplotlib
 
 apple = yf.Ticker("AAPL")
 
 with open('apple.json') as json_file:
     apple_info = json.load(json_file)
     # print the type of data variable
-    print((apple_info))
-# apple_info
+    # print((apple_info))
+# print(apple_info['country'])
+apple_share_price_data = apple.history(period="max")
+(apple_share_price_data.head())
+apple_share_price_data.reset_index(inplace=True)
+(apple_share_price_data.plot(x="Date", y="Open"))
