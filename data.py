7b20d1ee92c9adf6060b340a9c49ae6788de2d68
Joyce <joyceyukang@gmail.com>
2023-10-03 16:20:14 -0700

    databse

--- data.py
+++ data.py
@@ -4,13 +4,15 @@ import json
 import matplotlib
 
 apple = yf.Ticker("AAPL")
+print(apple)
 
 with open('apple.json') as json_file:
     apple_info = json.load(json_file)
     # print the type of data variable
     # print((apple_info))
 # print(apple_info['country'])
-apple_share_price_data = apple.history(period="max")
-(apple_share_price_data.head())
-apple_share_price_data.reset_index(inplace=True)
-(apple_share_price_data.plot(x="Date", y="Open"))
+# apple_share_price_data = apple.history(period="max")
+# (apple_share_price_data.head())
+# apple_share_price_data.reset_index(inplace=True)
+# (apple_share_price_data.plot(x="Date", y="Open"))
+apple.dividends
